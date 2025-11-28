82fdc6389a12363885ed40332d812f2e176ebf3c
cc chennnnnn <97044221+ccccc411@users.noreply.github.com>
2025-11-28 22:39:35 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -150,7 +150,7 @@ function isInsideBounds(x, y, b) {
 function handlePointer(px, py) {
   for (let blob of blobs) {
     if (blob.bounds && isInsideBounds(px, py, blob.bounds)) {
-      window.location.href = `./line.html#${blob.id}`;
+      window.location.href = `./line.html?id=${blob.id}`;
       break;
     }
   }
@@ -168,6 +168,7 @@ function touchStarted() {
   handlePointer(tx, ty);
   return false;
 }
+
 
 
 
